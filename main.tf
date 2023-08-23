2233f84ff65a0076e4fba9ef477953a59266352d
Lalitha <lalitha.d339@gmail.com>
2023-08-23 05:31:50 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 module "components"{
 
-
-  source          ="git::https://github.com/LalithaDendukuri/basic-tf.git"
-  for_each        = var.components
-  zone_id         = var.zone_id
-  security_group  = var.security_group
-  name            = each.value["name"]
-  instance_type   = each.value["instance_type"]
+  source          ="git::https://github.com/LalithaDendukuri/tf-module-vpc.git"
+  for_each        = var.vpc
+  cidr            = each.value["cidr"]
 }
 
 
--- vars.tf
+++ vars.tf
@@ -1,4 +1,1 @@
-variable "components" {}
-
-variable "zone_id" {}
-variable "security_group" {}
+variable "vpc" {}

40d916ab3734cc9650d2a11b517d34bd941f51db
Lalitha <lalitha.d339@gmail.com>
2023-08-29 04:59:51 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-module "components"{
+module "vpc"{
 
   source          ="git::https://github.com/LalithaDendukuri/tf-module-vpc.git"
   for_each        = var.vpc
   cidr            = each.value["cidr"]
+  subnets         = each.value["subnets"]
 }
 
 

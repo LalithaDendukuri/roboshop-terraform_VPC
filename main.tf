0f7b00f0ce4132072190159ba5c651df7df5c2dd
Lalitha <lalitha.d339@gmail.com>
2023-09-02 05:29:30 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ module "alb" {
   internal        = each.value["internal"]
   lb_type         = each.value["lb_type"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  vpc_id     = each.value["internal"] ? local.vpc_id: var.default_vpc_id
-  subnets     =  each.value["internal"] ? local.app_subnet : data.aws_subnets.subnets.ids
-  sg_port =each.value["sg_port"]
+  vpc_id          = each.value["internal"] ? local.vpc_id: var.default_vpc_id
+  subnets         = each.value["internal"] ? local.app_subnet : data.aws_subnets.subnets.ids
+  sg_port         = each.value["sg_port"]
   tags            = var.tags
-  env = var.env
+  env             = var.env
 }

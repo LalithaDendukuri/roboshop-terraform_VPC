87c5d1bedf8c99ccad51e058ebc7991d50e0935d
Lalitha <lalitha.d339@gmail.com>
2023-09-01 18:24:27 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 module "vpc" {
   source          ="git::https://github.com/LalithaDendukuri/tf-module-vpc.git"
+
   for_each        = var.vpc
   cidr            = each.value["cidr"]
   subnets         = each.value["subnets"]
   default_vpc_id  =var.default_vpc_id
-  default_vpc_cidr = var.default_vpc_cidr
+  default_vpc_cidr       = var.default_vpc_cidr
   default_route_table_id = var.default_route_table_id
-  tags            = var.tags
-  env = var.env
+  tags                  = var.tags
+  env                   = var.env
 }
 output "vpc" {
   value = module.vpc

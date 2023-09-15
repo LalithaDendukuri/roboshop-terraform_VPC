e96538726902500ec514d02e056e8f8c32074e87
Lalitha <lalitha.d339@gmail.com>
2023-09-16 04:21:50 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -107,6 +107,7 @@ module "rabbitmq" {
 }
 
 
+/*
 
 
 module "app" {
@@ -133,4 +134,4 @@ module "app" {
 
   public_alb_name = lookup(lookup(lookup( module.alb, "public",null),"alb",null),"dns_name",null)
   public_listener = lookup(lookup(lookup( module.alb, "public",null),"listener",null),"arn",null)
-}
+}*/
